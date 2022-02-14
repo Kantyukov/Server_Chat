8358c298dfb60358e1ada993037c4479f51754d9
Kantyukov Artem <Tema_genius@mail.ru>
2022-02-14 17:11:34 +0300

    Initial commit

---
+++ src/main/java/ru/artem/kantyukov/Application.java
@@ -0,0 +1,30 @@
+package ru.artem.kantyukov;
+
+import com.mysql.cj.xdevapi.Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Application {
+    public static final int PORT =  8081;
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("==== Server connected ====" );
+        while (true){
+            Socket socket = serverSocket.accept();
+
+            if (socket!=null){
+                System.out.println("Client connected");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println(bufferedReader.readLine());
+            }
+
+            }
+        }
+    }
+
